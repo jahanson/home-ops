0bc0fb0b653941fe6e14aeab25772098c768ff3d
Joseph Hanson <joe@veri.dev>
2023-06-14 10:03:43 -0500

    Updating flux to rc.5.

--- infrastructure/talos/morgoth/apply.sh
+++ infrastructure/talos/morgoth/apply.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 # Deploy the configuration to the nodes
-talosctl apply-config -n 10.1.1.61 -f ./clusterconfig/morgoth-cirdan.jahanson.tech.yaml
+talosctl apply-config -n 10.1.1.61 -f ./clusterconfig/morgoth-galadriel.jahanson.tech.yaml
 talosctl apply-config -n 10.1.1.62 -f ./clusterconfig/morgoth-elrond.jahanson.tech.yaml
-talosctl apply-config -n 10.1.1.63 -f ./clusterconfig/morgoth-galadriel.jahanson.tech.yaml
-talosctl apply-config -n 10.1.1.41 -f ./clusterconfig/morgoth-narya.jahanson.tech.yaml
-talosctl apply-config -n 10.1.1.42 -f ./clusterconfig/morgoth-nenya.jahanson.tech.yaml
-talosctl apply-config -n 10.1.1.43 -f ./clusterconfig/morgoth-vilya.jahanson.tech.yaml
+talosctl apply-config -n 10.1.1.63 -f ./clusterconfig/morgoth-cirdan.jahanson.tech.yaml
+
+talosctl apply-config -n 10.1.0.220 -f ./morgoth-nenya.jahanson.tech.yaml --insecure
+talosctl apply-config -n 10.1.0.222 -f ./morgoth-vilya.jahanson.tech.yaml --insecure
+talosctl apply-config -n 10.1.0.221 -f ./morgoth-narya.jahanson.tech.yaml --insecure
